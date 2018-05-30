bb3b5d4ec260dbeb6790a2d73c1fe77d827ac3ca
Tomas Stibrany <tomasstibrany@Tomass-MacBook-Pro.local>
2018-05-30 20:46:49 +0200

    Fourth attempt at drawing line, All 12 lines appeared

--- TinyRendererLib/Renderer.cs
+++ TinyRendererLib/Renderer.cs
@@ -27,34 +27,24 @@ namespace TinyRendererLib
 		/// <param name="p1y">End point's Y-Coordinate</param>
 		public void DrawLine(int p0x, int p0y, int p1x, int p1y)
 		{
-            // swap points to make sure we draw from left to right
-            if (p0x > p1x)
-            {
-                // swap X coordinates 
-                int tmp = p0x;
-                p0x = p1x;
-                p1x = tmp;
-                // swap Y coordinates 
-                tmp = p0y;
-                p0y = p1y;
-                p1y = tmp;
-            }
-
             int deltaY = p1y - p0y;
             int deltaX = p1x - p0x;
 
-			// slope is the "k" in the formula
             double slope = (deltaX == 0)
                 ? 0 // Handle vertical lines 
                 : ((double)deltaY) / ((double)deltaX);
 
 			// To calculate q (y-intercept) we can use either of the points
-			double yIntercept = (double)p0y - slope * (double)p0x;
+            double yIntercept = (double)p0y - slope * (double)p0x;
 
             // Iterate over axis with more points 
+            int axisIncrement;
+            int totalSteps;
 			if (Math.Abs(deltaX) >= Math.Abs(deltaY))
             {
-                for (int x = p0x; x <= p1x; x++)
+                axisIncrement = (p0x <= p1x ? 1 : -1); // Calculate positive or negative value which depends on the direction of the drawing
+				totalSteps = Math.Abs(p0x - p1x); // Calculate total number of pixels needed to be drawn for the line
+                for (int step = 0, x = p0x; step <= totalSteps; x += axisIncrement, step++)
                 {
 					double y = slope * (double)x + yIntercept;
 					drawPixel(x, (int)Math.Round(y));
@@ -62,11 +52,14 @@ namespace TinyRendererLib
             }
             else
             {
-                for (int y = p0y; y <= p1y; y++)
+                axisIncrement = (p0y <= p1y ? 1 : -1); // Calculate positive or negative value which depends on the direction of the drawing
+				totalSteps = Math.Abs(p0y - p1y); // Calculate total number of pixels needed to be drawn for the line
+                for (int step = 0, y = p0y; step <= totalSteps; y += axisIncrement, step++)
                 {
                     double x = (deltaX != 0)
 						? ((double)y - yIntercept) / slope
                         : p0x; // handle vertical line 
+
 					drawPixel((int)Math.Round(x), y);
                 }
             }
